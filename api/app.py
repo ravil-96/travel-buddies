5417c341eb333931fa739685e24e1a7149caf748
chris westerduin <chriswesterduin@outlook.com>
2021-05-13 20:50:57 +0100

    initial setup with docker

--- api/app.py
+++ api/app.py
@@ -1,14 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route('/')
-def home():
-    return jsonify({'message': 'Hello from Liverpool Football API!'}), 200
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def hello():
+    return 'Hello World'
