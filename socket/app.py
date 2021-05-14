1a798975aedfeebe01365fc98349e2a86f4ff079
chris westerduin <chriswesterduin@outlook.com>
2021-05-14 11:17:27 +0100

    socket server working, sends and recieves data

--- socket/app.py
+++ socket/app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+def handle_message(message):
+    send(message)
+
+@socketio.on('json')
+def handle_json(json):
+    send(json, json=True)
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    emit('my response', json, broadcast=True)
+
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port="3000")
