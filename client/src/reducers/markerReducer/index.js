615816e3f9f37661e64308f9cdd6f75cb5f7142b
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-14 10:42:59 +0100

    add basic filestructure for Redux and initial state for map markers

---
+++ client/src/reducers/markerReducer/index.js
@@ -0,0 +1,10 @@
+const mapMarkerState = {
+    markers: [
+        { id: 0, name: 'Empire State Building', comment: '', username: 'Beth'},
+        { id: 1, name: 'Rockefeler Centre', comment: '', username: 'Michael'},
+        { id: 2, name: 'Brooklyn Bridge', comment: '', username: 'Semhar'}
+    ],
+    filters: {
+        date: ['oldest', 'latest']
+    }
+} 
