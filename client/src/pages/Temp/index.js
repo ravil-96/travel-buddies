7fabc666838be43ee45f89863abaa312c36cecf2
chris westerduin <chriswesterduin@outlook.com>
2021-05-14 11:28:08 +0100

    API requests working from client

--- client/src/pages/Temp/index.js
+++ client/src/pages/Temp/index.js
@@ -1,25 +1,34 @@
-import React, { useState, useEffect } from'react';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import axios from "axios";
 
-function Temp(){
-    const [mySocket, setMySocket] = useState(null)
-    useEffect(() => {
-        const socket = io("localhost:3000");
-        console.log("connected", socket)
-        setMySocket(socket)
+function Temp() {
+  const [mySocket, setMySocket] = useState(null);
+  useEffect(() => {
+    const socket = io("localhost:3000");
+    console.log("connected", socket);
+    setMySocket(socket);
 
-        socket.on('my response', (data) => {
-            console.log(data)
-        });
-      },[])
+    socket.on("my response", (data) => {
+      console.log(data);
+    });
+  }, []);
 
-      function handleClick(){
-        mySocket.emit("my event", {msg: 'hello!'});
-      }
-    return( <>
-            <h1>Temp page</h1>
-            <button onClick={handleClick}>click</button>
-            </>)
+  function handleClick() {
+    mySocket.emit("my event", { msg: "hello!" });
+  }
+
+  async function handlePost() {
+    let data = await axios.post("http://localhost:5000/test");
+    console.log(data);
+  }
+  return (
+    <>
+      <h1>Temp page</h1>
+      <button onClick={handleClick}>test socket</button>
+      <button onClick={handlePost}>test API</button>
+    </>
+  );
 }
 
-export default Temp
+export default Temp;
