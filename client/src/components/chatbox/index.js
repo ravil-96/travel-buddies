83c6f5845dd359b40c7465b1ca2d040a09f9b0f5
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-17 18:59:04 +0100

    update chatbox

--- client/src/components/chatbox/index.js
+++ client/src/components/chatbox/index.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
-// import openLauncher from "../../../public/images/launcher_button.png";
-// import close from '../../../public/images/clear-button.png';
-//  import sendButtonIcon from "../../../public/images/send_button.png";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "./index.css";
 
 function ChatBox() {
@@ -19,21 +18,29 @@ function handleInput(e){
 }
 
   return (
-      <div className="chatApp__room">
-        <div className="chatApp_conv">
-          <div className="chatApp_convTimeline">
+    <div className="chat-box">
+      <div className="chatApp_conv">
+        <div className="chatApp_convTimeline">
+          <div className="chatApp_convMessageItem--left clearfix">
+            <div className="chatApp__convMessageValue">hello?</div>
+          </div>
 
-            <div className="chatApp_convMessageItem--left">
-              <div className="chatApp__convMessageValue">hello?</div>
-            </div>
-
-            <div className="chatApp_convMessageItem--right">
-              <div className="chatApp__convMessageValue">hello</div>
-            </div>
-
+          <div className="chatApp_convMessageItem--right clearfix">
+            <div className="chatApp__convMessageValue">hello</div>
           </div>
         </div>
+        <div className=" row">
+          <Form className="col-10">
+            <Form.Group>
+              <Form.Control type="text" placeholder="Enter message" className="text-input"/>
+            </Form.Group>
+          </Form>
+          <Button variant="primary" type="submit" className="send-message btn col-2">
+            Send
+          </Button>
+        </div>
       </div>
+    </div>
   );
 }
 export default ChatBox;
