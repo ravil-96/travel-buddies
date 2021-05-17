887434f82986f24c96954caf9833a49477323089
chris westerduin <chriswesterduin@outlook.com>
2021-05-17 16:51:50 +0100

    style changes, add Marker cards

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -8,4 +8,5 @@ export { default as Weather } from "./Weather";
 export { default as WeatherIcon } from "./WeatherIcon";
 export { default as NewHolidayButton } from "./NewHolidayButton";
 export { default as HolidayCards } from "./HolidayCards";
+export { default as MarkerCards } from "./MarkerCards";
 export { default as CardContainer } from "./CardContainer";
--- client/src/pages/holiday/index.js
+++ client/src/pages/holiday/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux"
-import { MapSearch, Weather, MyMap, MarkerModal, CardContainer } from "../../components";
+import { MapSearch, Weather, MyMap, MarkerModal, CardContainer, MarkerCards } from "../../components";
 import { useParams } from "react-router-dom"
 import { useSocket } from '../../customHooks'
 import { clearMarkers, loadHoliday } from '../../actions'
@@ -37,11 +37,19 @@ function Holiday() {
 
   return (
     <>
-      <Weather />
-      <MyMap handleClick={handleClick}/>
-      <MarkerModal show={show} handleClose={handleClose} location={markerLocation} />
+      {/* <Weather /> */}
+      <MarkerModal
+        show={show}
+        handleClose={handleClose}
+        location={markerLocation}
+      />
       <MapSearch handleClick={handleClick} />
-      <CardContainer />
+      <div className="map-card-box">
+        <MyMap handleClick={handleClick}/>
+        <CardContainer>
+          <MarkerCards />
+        </CardContainer>
+      </div>
     </>
   );
 }
