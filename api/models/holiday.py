8d3ea5db0f0946311732893a7d94070184f4f776
chris westerduin <chriswesterduin@outlook.com>
2021-05-17 07:14:03 +0100

    add model/view for create marker

--- api/models/holiday.py
+++ api/models/holiday.py
@@ -3,7 +3,6 @@ from app import db
 class Holidays(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=False, nullable=False)
-    markers = db.relationship('Markers', backref='holiday_id', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.title
--- api/models/map.py
+++ api/models/map.py
@@ -4,5 +4,13 @@ class Markers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
+    position_lat = db.Column(db.Numeric, nullable=False)
+    position_long = db.Column(db.Numeric, nullable=False)
     holiday_id = db.Column(db.Integer, db.ForeignKey('holidays.id'),
         nullable=False)
+
+def create_marker(new_title, new_desc, new_position_lat, new_position_long, new_holiday_id):
+    marker = Markers(title=new_title, desc=new_desc, position_lat=new_position_lat, position_long=new_position_long, holiday_id=new_holiday_id)
+    db.session.add(marker)
+    db.session.commit()
+    return marker
