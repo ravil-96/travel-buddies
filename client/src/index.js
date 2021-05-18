fc15af59a5e5d934e8c079f24b0482eef1a48f5c
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-18 12:53:17 +0100

    add persist

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import store from "./store";
-import persistor  from "./store";
+// import {store} from "./store";
+// import {persistor}  from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
+import rootReducer from "./reducer";
 
 import App from "./App";
+
+const persistConfig = {
+  key: "user",
+  storage: storage,
+  whitelist: ["user"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+let store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,3 +42,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
+
--- client/src/store.js
+++ client/src/store.js
@@ -1,25 +1,26 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import logger from "redux-logger";
+// import { createStore, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import logger from "redux-logger";
 
 
-import rootReducer from "./reducer";
+// import rootReducer from "./reducer";
 
-// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+// // const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
-const persistedReducer = persistReducer(
-  persistConfig,
-  rootReducer);
-let store = createStore(
-  persistedReducer);
-let persistor = persistStore(store);
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
+// const persistedReducer = persistReducer(
+//   persistConfig,
+//   rootReducer);
+// let store = createStore(
+//   persistedReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+// );
+// let persistor = persistStore(store);
 
-export default { store, persistor }
+// export default { store, persistor }
 
