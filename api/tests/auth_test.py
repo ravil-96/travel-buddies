b5055bfef42687fc0b0e49d808e6c4a8b0290353
chris westerduin <chriswesterduin@outlook.com>
2021-05-19 13:55:31 +0100

    add integration tests

--- api/tests/auth_test.py
+++ api/tests/auth_test.py
@@ -1,26 +1,33 @@
 import json
+import pytest 
 
-def test_home(api):
-    res = api.get('/')
-    assert res.json == {'msg': 'hello'}
+@pytest.mark.usefixtures("teardown")
+class TestAuth():
+    def test_home(self, api):
+        res = api.get('/')
+        assert res.json == {'msg': 'hello'}
 
-def test_users(api):
-    res = api.get('/users')
-    assert res.json == {'users':[]}
+    def test_users(self, api):
+        res = api.get('/users')
+        assert len(res.json['users']) == 2
 
-def test_register(api):
-    mock_data = json.dumps({'username': 'admin', 'password':'password123', 'email':'test@test.com'})
-    mock_headers = {'Content-Type': 'application/json'}
-    res = api.post('/register', data=mock_data, headers=mock_headers)
-    assert res.json['msg'] == 'admin'
+    def test_register(self, api):
+        mock_data = json.dumps({'username': 'admin', 'password':'password123', 'email':'test@test.com'})
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/register', data=mock_data, headers=mock_headers)
+        assert res.json['msg'] == 'admin'
+    
+    def test_adds_user(self, api):
+        res = api.get('/users')
+        assert len(res.json['users']) == 3
 
-def test_login(api):
-    mock_data = json.dumps({'username': 'admin', 'password':'password123'})
-    mock_headers = {'Content-Type': 'application/json'}
-    res = api.post('/login', data=mock_data, headers=mock_headers)
-    assert res.json['msg'] == 'admin'
-    assert len(res.json['token']) > 0
+    def test_login(self, api):
+        mock_data = json.dumps({'username': 'admin', 'password':'password123'})
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/login', data=mock_data, headers=mock_headers)
+        assert res.json['msg'] == 'admin'
+        assert len(res.json['token']) > 0
 
-def test_users_search(api):
-    res = api.get('/users/search/a')
-    assert res.json['users'][0]['username'] == 'admin'
+    def test_users_search(self, api):
+        res = api.get('/users/search/a')
+        assert res.json['users'][0]['username'] == 'admin'
---
+++ api/tests/holiday_test.py
@@ -0,0 +1,47 @@
+import json
+import pytest 
+
+@pytest.mark.usefixtures("teardown")
+class TestHoliday():
+
+    def test_create_holiday(self, api):
+        mock_data = json.dumps({"title": "Test Holiday", "creator": 1})
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/holidays', data=mock_data, headers=mock_headers)
+        assert res.json['msg'] == 'success'
+        assert type(res.json['id']) == int
+    
+    def test_create_marker(self, api):
+        mock_data = json.dumps({
+                "title": "Test Marker",
+                "desc": "my test marker",
+                "position_lat": 0,
+                "position_long": 0,
+                "holiday_id": 1})
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/maps/marker', data=mock_data, headers=mock_headers)
+        assert type(res.json['id']) == int
+    
+    def test_get_holiday(self, api):
+        res = api.get('/holidays/1')
+        assert res.json['holiday']['title'] == 'Test Holiday'
+        assert res.json['markers'][0]['title'] == 'Test Marker'
+
+    
+    def test_get_user_holidays(self, api):
+        res = api.get('/users/1/holidays')
+        assert res.json['holidays'][0]['title'] == 'Test Holiday'
+        assert res.json['markers'][0]['title'] == 'Test Marker'
+
+    def test_add_user_to_holiday(self, api):
+        mock_data = json.dumps({"user_id": 1})
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/holidays/2/users', data=mock_data, headers=mock_headers)
+        assert res.json['data'] == 'success' 
+    
+    def test_get_holiday_users(self, api):
+        res = api.get('/holidays/1/users')
+        assert res.json['creator']['user_id'] == 1
+        assert res.json['users'][0]['user_id'] == 2
+
+
