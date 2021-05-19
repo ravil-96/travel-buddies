3f7865a9b39b336bc0e388e255a2333944eceaa0
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-19 16:31:27 +0100

    add footer test

---
+++ client/src/pages/holiday/Holiday.test.js
@@ -0,0 +1,17 @@
+import Holiday from "./index";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+
+// describe("Holiday", () => {
+//   beforeEach(() => {
+    
+//     renderWithReduxProvider(<MarkerCards />);
+//   });
+
+//   test("it renders a marker card", () => {
+//     expect(
+//       screen.getByRole("markercards", { hidden: true })
+//     ).toBeInTheDocument();
+//   });
+// });
