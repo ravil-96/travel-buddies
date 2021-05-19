a5b2a77950b1ff9b4931de7a9338f2325693e87d
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-19 10:54:09 +0100

    add tests to pagenotfound, home and profile

--- client/src/pages/pageNotFound/index.js
+++ client/src/pages/pageNotFound/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PageNotFound() {
   return (
-    <div>
+    <div role="pageNotFound">
       <h1>Oops!</h1>
       <h3> You might be lost, return to the home page ... </h3>
     </div>
