7c5b08ff52a8506ea5ececc16340991d8436a580
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-19 17:13:50 +0100

    add weather api

--- client/src/components/Weather/index.js
+++ client/src/components/Weather/index.js
@@ -4,85 +4,85 @@ import WeatherIcon from "../WeatherIcon";
 import { useSelector, useDispatch } from "react-redux";
 import {MarkerModal, MapSearch} from '../MarkerModal'
 import {Holiday} from '../../pages'
-const apiKey = "d4ef3671c18ff18482439536bbb0fa40";
+// const apiKey = "d4ef3671c18ff18482439536bbb0fa40";
 
 function Weather() {
   const [city, setCity] = useState("");
-  const [markerLocation, setMarkerLocation] = useState(['',''])
+  // const [markerLocation, setMarkerLocation] = useState(['',''])
 
-  const markers = useSelector((state) => state.markers);
-  const [position, setPosition] = useState([])
-  const [weatherData, setWeatherData] = useState(null)
+  // const markers = useSelector((state) => state.markers);
+  // const [position, setPosition] = useState([])
+  const [weatherData, setWeatherData] = useState("")
 
-  useEffect(() => {
-    if (markers.length > 0) {
-      setPosition([ markers[0].position[0], markers[0].position[1] ])
-    }
-  },[markers])
+  // useEffect(() => {
+  //   if (markers.length > 0) {
+  //     setPosition([ markers[0].position[0], markers[0].position[1] ])
+  //   }
+  // },[markers])
 
-  useEffect(() => {
-    if (position.length > 0 && weatherData === null) {
-    async function fetchWeather(){
-      let geoApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position[0]}&lon=${position[1]}&units=metric&appid=${apiKey}`;
-      const { data } = await axios.get(geoApiUrl)
-      return data
-      }
-      setWeatherData(fetchWeather())
-      console.log(weatherData)
-    }
-  },[position])
+  // useEffect(() => {
+  //   if (position.length > 0 && weatherData === null) {
+  //   async function fetchWeather(){
+  //     let geoApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position[0]}&lon=${position[1]}&units=metric&appid=${apiKey}`;
+  //     const { data } = await axios.get(geoApiUrl)
+  //     return data
+  //     }
+  //     setWeatherData(fetchWeather())
+  //     console.log(weatherData)
+  //   }
+  // },[position])
 
  
  
-//   const title = useSelector((state) => state.markers.title);
+  const title = useSelector((state) => state.markers.title);
 // console.log(lat)
 // console.log(long);
-// console.log(title)
+console.log(title)
 
-  const apiKey = "d4ef3671c18ff18482439536bbb0fa40";
+  const apiKey = "0332bea479abd00765b838a8f968ed52";
 
 
 
-  // function search() {
+  function search() {
   //   let geoApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
-  //   // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  //   axios.get(geoApiUrl).then(getWeather);
-  // }
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-  
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(getWeather);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+  search()
   //   setCity(title)
-  // }
+  }
 
-  // function updateCity(event) {
-  //   event.preventDefault();
-  //   setCity(event.target.value);
-  // }
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
 
-  // function getWeather(response) {
-  //   setWeatherData({
-  //     temperature: Math.round(response.data.main.temp),
-  //     icon: response.data.weather[0].icon,
-  //     city: response.data.name,
-  //     description: response.data.weather[0].description,
-  //   });
-  //   console.log(temperature);
-  // }
+  function getWeather(response) {
+    setWeatherData({
+      temperature: Math.round(response.data.main.temp),
+      icon: response.data.weather[0].icon,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+    });
+    console.log(temperature);
+  }
   // search();
   return (
     <div>
-      {/* <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="search"
           placeholder="Type a city..."
           onChange={updateCity}
         />
         <input type="submit" value="Search" />
-      </form> */}
+      </form>
       <p>
-        {/* Current weather in {weatherData.city} is {weatherData.description}{" "}
+        Current weather in {weatherData.city} is {weatherData.description}{" "}
         {weatherData.temperature}°{" "}
-        <WeatherIcon code={weatherData.icon} alt={weatherData.description} /> */}
+        <WeatherIcon code={weatherData.icon} alt={weatherData.description} />
         hello!
       </p>
     </div>
