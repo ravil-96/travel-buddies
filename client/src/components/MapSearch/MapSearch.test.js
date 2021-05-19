90461246f4533598646a6012a8268bdb97cb6304
sem-the-dev <semhar.tesfu@outlook.com>
2021-05-19 15:58:59 +0100

    add mapsearch axios test

--- client/src/components/MapSearch/MapSearch.test.js
+++ client/src/components/MapSearch/MapSearch.test.js
@@ -1,27 +1,62 @@
 import MapSearch from "./index";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import mockedAxios from "axios";
+
+afterEach(cleanup);
 
 describe("MapSearch", () => {
   beforeEach(() => {
     renderWithReduxProvider(<MapSearch />);
   });
-  //   test("it renders a dropdown search menu", () => {
-  //     const stubMapSearch = jest.fn();
-  //     render(<MapSearch handleMapSearch={stubMapSearch} />);
-  //     let btn = screen.getByRole("DropdownButton");
-  //     userEvent.click(btn);
-  //     expect(stubMapSearch).toHaveBeenCalledTimes(1);
-  //   });
+//   test("it renders a dropdown search menu", () => {
+//     const stubMapSearch = jest.fn();
+//     render(<MapSearch handleMapSearch={stubMapSearch} />);
+//     let btn = screen.getByRole("button", { name: "dropdown search" });
+//     userEvent.click(btn);
+//     expect(stubMapSearch).toHaveBeenCalledTimes(1);
+//   });
 
   test("it renders a drop down search button", () => {
     expect(
       screen.getByRole("button", { name: "Search destination" })
     ).toBeInTheDocument();
   });
-    // test("it renders a search form", () => {
-    //   expect(
-    //     screen.getByPlaceholderText("form", { name: "Enter location" })
-    //   ).toBeInTheDocument();
-    // });
+
+  test("change values of form input ", () => {
+    const handleChange = jest.fn();
+    const { container } = render(
+      <input type="search" onChange={handleChange} />
+    );
+    const input = container.firstChild;
+    fireEvent.change(input, { target: { value: "a" } });
+    expect(handleChange).toHaveBeenCalledTimes(1);
+    expect(input.value).toBe("a");
+  });
+
+  test("fetch location axios request", async () => {
+    const data = {
+      data: [
+        {
+          city: "London",
+        },
+        {
+          city: "Glasgow",
+        },
+        {
+          city: "New York",
+        },
+      ],
+    };
+    mockedAxios.get.mockResolvedValueOnce(data);
+    const { getByText } = render(<MapSearch />);
+    await (() => {
+      expect(getByText("New York"));
+    });
+  });
 });
