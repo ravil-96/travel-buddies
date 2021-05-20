a6ab55b0d92d248bbdbe86fd61a9368d1455bd1f
chris westerduin <chriswesterduin@outlook.com>
2021-05-20 15:49:28 +0100

    production API

--- api/views/map.py
+++ api/views/map.py
@@ -3,11 +3,12 @@ from flask import request, jsonify
 from models import Markers, create_marker, delete_marker
 from app import app
 
-# maps data routes
 @app.route('/maps/marker', methods=['POST'])
 def create_marker_route():
     data = request.get_json()
-    marker = create_marker(data['title'],data['desc'],data['position_lat'], data['position_long'], data['holiday_id'])
+    marker = create_marker(data['title'], data['desc'],
+                           data['position_lat'], data['position_long'],
+                           data['holiday_id'])
     return jsonify({**data, "id": marker.id})
 
 @app.route('/maps/marker/<id>', methods=['DELETE'])
